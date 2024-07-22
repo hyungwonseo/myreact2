448aa2858692f28a4a21f3cf7214bf511ab47dd0
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-07-22 14:45:47 +0900

    update

---
+++ src/Components/20240722/Test1.js
@@ -0,0 +1,9 @@
+import "./Test1.css";
+
+export function Test1() {
+  return (
+    <>
+      <div className="box">Test1</div>
+    </>
+  );
+}
---
+++ src/Components/20240722/Test2.js
@@ -0,0 +1,9 @@
+import "./Test2.css";
+
+export function Test2() {
+  return (
+    <>
+      <div className="box">Test2</div>
+    </>
+  );
+}
