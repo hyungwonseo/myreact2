348df21e658714da653934fac098fc64263561d5
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-07-24 11:23:27 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import { Gallery } from "./Components/20240723/Gallery";
+import { createGlobalStyle } from "styled-components";
+import { Home } from "./Components/20240724/Home";
+
+const GlobalStyle = createGlobalStyle`
+@font-face {
+    font-family: 'GmarketSansMedium';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: GmarketSansMedium;
+  }
+`;
 
 function App() {
   return (
     <>
-      <Gallery />
+      <GlobalStyle />
+      <Home />
     </>
   );
 }
