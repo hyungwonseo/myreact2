199bf500037e7c1d4839f6e653c952f6a5d57e47
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-07-25 15:59:50 +0900

    update

--- src/Components/MovieShop/MovieList.js
+++ src/Components/MovieShop/MovieList.js
@@ -1,7 +1,26 @@
-import { useEffect } from "react";
-import { getMoviesNowPlaying } from "./api";
+import { useEffect, useState } from "react";
+import { getMoviesNowPlaying, getGenre, IMG_PATH } from "./api";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 10px;
+`;
+const Card = styled.div`
+  width: 100%;
+  border: 1px solid dodgerblue;
+  cursor: pointer;
+`;
+const Img = styled.img`
+  width: 100%;
+`;
+const Text = styled.div``;
 
 export function MovieList() {
+  const [data, setData] = useState(null);
+
   useEffect(() => {
     getMovies();
   }, []);
@@ -10,6 +29,7 @@ export function MovieList() {
     try {
       const response = await getMoviesNowPlaying();
       console.log(response.data);
+      setData(response.data);
     } catch (error) {
       console.log("Error", error);
     }
@@ -18,6 +38,17 @@ export function MovieList() {
   return (
     <>
       <h1>Movie List</h1>
+      <Container>
+        {data &&
+          data.results.map((movie) => (
+            <Card key={movie.id}>
+              <Img src={IMG_PATH + movie.poster_path}></Img>
+              <Text>타이틀 : {movie.title}</Text>
+              <Text>장르 : {getGenre(movie.genre_ids)}</Text>
+              <Text>소개글 : {movie.overview}</Text>
+            </Card>
+          ))}
+      </Container>
     </>
   );
 }
--- src/Components/MovieShop/api.js
+++ src/Components/MovieShop/api.js
@@ -8,9 +8,99 @@ const headers = {
   },
 };
 
+export const IMG_PATH = "https://image.tmdb.org/t/p/original";
+
 export function getMoviesNowPlaying() {
   return axios.get(
-    "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+    "https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&page=1",
     headers
   );
+}
+
+export const genre = [
+  {
+    id: 28,
+    name: "Action",
+  },
+  {
+    id: 12,
+    name: "Adventure",
+  },
+  {
+    id: 16,
+    name: "Animation",
+  },
+  {
+    id: 35,
+    name: "Comedy",
+  },
+  {
+    id: 80,
+    name: "Crime",
+  },
+  {
+    id: 99,
+    name: "Documentary",
+  },
+  {
+    id: 18,
+    name: "Drama",
+  },
+  {
+    id: 10751,
+    name: "Family",
+  },
+  {
+    id: 14,
+    name: "Fantasy",
+  },
+  {
+    id: 36,
+    name: "History",
+  },
+  {
+    id: 27,
+    name: "Horror",
+  },
+  {
+    id: 10402,
+    name: "Music",
+  },
+  {
+    id: 9648,
+    name: "Mystery",
+  },
+  {
+    id: 10749,
+    name: "Romance",
+  },
+  {
+    id: 878,
+    name: "Science Fiction",
+  },
+  {
+    id: 10770,
+    name: "TV Movie",
+  },
+  {
+    id: 53,
+    name: "Thriller",
+  },
+  {
+    id: 10752,
+    name: "War",
+  },
+  {
+    id: 37,
+    name: "Western",
+  },
+];
+
+export function getGenre(list) {
+  let str = "";
+  list.forEach((a) => {
+    const temp = genre.find((g) => g.id == a);
+    str = str + ", " + temp.name;
+  });
+  return str;
 }
