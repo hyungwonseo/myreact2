59218a1114e5f6bcbe99181564b46d93bc0c8692
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-07-26 11:23:04 +0900

    Update Movie.js

--- src/Components/MovieShop/Movie.js
+++ src/Components/MovieShop/Movie.js
@@ -1,8 +1,29 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getMovieDetailById, getMovieCreditById } from "./api";
 
 export function Movie() {
   const { id } = useParams();
-  console.log(id);
+  const [detail, setDetail] = useState(null);
+  const [credit, setCredit] = useState(null);
+
+  useEffect(() => {
+    getMovieInfo();
+  }, []);
+
+  async function getMovieInfo() {
+    try {
+      let response = await getMovieDetailById(id);
+      console.log(response);
+      setDetail(response.data);
+      response = await getMovieCreditById(id);
+      console.log(response);
+      setCredit(response.data);
+    } catch (error) {
+      console.log("Error", error);
+    }
+  }
+
   return (
     <>
       <h1>Movie : {id}</h1>
